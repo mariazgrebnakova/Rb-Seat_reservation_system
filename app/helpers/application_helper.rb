89ecef8020c839b7cdf662fc7b8d2a0b39c9a85d
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2021-11-17 11:56:44 +0100

    Fix alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def flash_class
+        case key
+        when 'notice'
+            'info'
+        when 'alert'
+            'warning'
+        else
+            key
+        end
+    end
 end
