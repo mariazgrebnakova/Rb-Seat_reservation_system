ac24a7d981d8900682ea1fae7123c221e772ae9b
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2021-11-28 13:09:01 +0100

    Add restrictions to normal users, create admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_205549) do
+ActiveRecord::Schema.define(version: 2021_11_28_111247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_205549) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "is_admin", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
