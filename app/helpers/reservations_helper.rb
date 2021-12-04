0e687f823e7b5b45545af71e0e98dba8d91495c6
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2021-12-04 20:41:31 +0100

    Display full name to reservations form method

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,3 +1,4 @@
 module ReservationsHelper
+    
    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
