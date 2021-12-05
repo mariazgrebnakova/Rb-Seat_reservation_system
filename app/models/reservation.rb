89770b6013f4b608807790e6335e777a87acb310
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2021-12-05 12:01:02 +0100

    Add validation to check that user has only one reservation per date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
 
     validate :from_must_be_before_to_date
     validate :validate_seat_is_empty
+    validate :validates_user_has_no_reservation
 
     private
 
@@ -24,7 +25,7 @@ class Reservation < ApplicationRecord
     end
 
     def validate_seat_is_empty
-        errors.add(:seat_id, "Seat is not available in this date range") unless
+        errors.add(:seat_id, "Seat is taken") unless
             is_seat_empty == true
     end
 
@@ -39,6 +40,37 @@ class Reservation < ApplicationRecord
             end
         end
         return does_not_include
+    end
+
+    def user_reservations_dates
+        user_reservations_dates = []
+        choosed_user = user_id
+        Reservation.where(user_id: choosed_user).each do |range|
+            (range.from..range.to).each do |date|
+                user_reservations_dates << date
+            end 
+        end
+        puts user_reservations_dates
+        return user_reservations_dates
+    end
+
+    def user_has_no_reservation
+        has_no_reservation = true
+        reservation_dates = user_reservations_dates
+        (from..to).each do |date|
+            if  reservation_dates.nil?
+                has_no_reservation = true
+            elsif date.in? reservation_dates
+                has_no_reservation = false
+            end
+        end
+        puts has_no_reservation
+        return has_no_reservation
+    end
+
+    def validates_user_has_no_reservation
+        errors.add(:user_id, "More than one reservation by user on this date") unless
+            user_has_no_reservation == true
     end
 
 end
