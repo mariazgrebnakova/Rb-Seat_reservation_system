0bb3a851e55243f5a9ea4f777f0ebc8062eaac39
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2021-12-29 21:24:54 +0100

    Add map with colored seats to reservations

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,4 +1,6 @@
 module ReservationsHelper
-    
+    def seat_color
+        return "{fillColor:'880808'}"
+    end
    
 end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,4 +1,13 @@
 class Seat < ApplicationRecord
     belongs_to :department
     has_many :reservations
+
+    def seat_color
+        if id == 2
+            return "{fillColor:'FFFF00'}"
+        else
+            return "{fillColor:'800080'}"
+        end
+    end
+
 end
