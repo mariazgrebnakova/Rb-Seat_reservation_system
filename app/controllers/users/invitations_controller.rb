636207d83b851dceb6ea790701e4477ca4a04e10
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-01-14 23:23:08 +0100

    Add automatic parameter of user_type for new users

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -5,7 +5,7 @@ class Users::InvitationsController < Devise::InvitationsController
   
     # Permit the new params here.
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:invite, keys: [:email, :first_name, :last_name, :department_id])
+      devise_parameter_sanitizer.permit(:invite, keys: [:email, :first_name, :last_name, :department_id, :user_type_id])
     end
 
 end
