fa7d5d2bb468e286e24bdd51dccb8d0124244ff4
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-01-16 14:11:57 +0100

    Add email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :user_type_id, presence: true
 
   def full_name
