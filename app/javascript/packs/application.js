54607f5e46037d4d8f83bf7ff8144146ecc24488
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-01-27 21:42:29 +0100

    Add disabling of reserve button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,18 +28,31 @@ $(function () {
     // Na elemente `area` pod elementom `map` pocuvam na `click` event
     $('map area.is_seat_free').click(function () {
         let seat_id = $(this).data('id')
-
-        $('[name=seat_id]').val(seat_id)
-
+        
         var data = $(this).data('maphilight') || {};
         if (!jQuery.isEmptyObject(data)) {
             data = {}
+            $('[name=seat_id]').val(null)
         } else {
             data.fillColor = '00ffff'; // svetlomodra
+            $('[name=seat_id]').val(seat_id)
         }
+        $('#seat_id').trigger('change')
 
         // This sets the new data, and finally checks for areas with alwaysOn set
         $('map area').data('maphilight', {}).trigger('alwaysOn.maphilight');
         $(this).data('maphilight', data).trigger('alwaysOn.maphilight');
+    })
+
+    $('#reserve_button').attr("disabled", true)
+
+    $('[name=seat_id]').change(function () {
+        let seat_id = $(this).val()
+
+        if (seat_id == "") {
+            $('#reserve_button').attr("disabled", true)
+        } else {
+            $('#reserve_button').attr("disabled", false)
+        }
     })
 });
