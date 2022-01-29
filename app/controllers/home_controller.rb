6a7683e49b1df88c0d4e85373b673cd458a11f9c
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-01-29 11:17:17 +0100

    Add reservations calendar to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   
   def index
     @current_user_reservations = Reservation.all.where(user_id: current_user.id).where('"from" >= ?', Date.current).order(:from).page(params[:page]).per(5)
+    @meetings = Reservation.where(user_id: current_user.id)
   end
 end
