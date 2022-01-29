3b044f6779b8f999ad32fb9e5961f2f96e4f2baa
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-01-29 13:33:35 +0100

    Optimalize loaded scope

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   
   def index
     @current_user_reservations = Reservation.all.where(user_id: current_user.id).where('"from" >= ?', Date.current).order(:from).page(params[:page]).per(5)
-    @meetings = Reservation.where(user_id: current_user.id)
+    searched_month = params[:start_date].present? ? params[:start_date].to_date.month: Date.current.month
+    @meetings = Reservation.where(user_id: current_user.id).where("EXTRACT (MONTH FROM \"from\") = #{searched_month}")
   end
 end
