1757f45ae27559760474b074983d568eecbe3da9
Maria Zgrebnakova <maria.zgrebnakova@gmail.com>
2022-02-02 18:02:21 +0100

    Fix style sheets and reservations mapping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   
   def index
     @current_user_reservations = Reservation.all.where(user_id: current_user.id).where('"from" >= ?', Date.current).order(:from).page(params[:page]).per(5)
-    searched_month = params[:start_date].present? ? params[:start_date].to_date.month: Date.current.month
-    @meetings = Reservation.where(user_id: current_user.id).where("EXTRACT (MONTH FROM \"from\") = #{searched_month}")
+    searched_date = params[:start_date].present? ? params[:start_date].to_date: Date.current
+    @meetings = Reservation.where(user_id: current_user.id).where("\"from\" BETWEEN '#{40.days.before(searched_date)}' AND '#{40.days.after(searched_date)}'")
   end
 end
